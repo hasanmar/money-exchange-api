473fd4e46d0feb6f1fcddbf8f32ec97d76b87c5a
Aref Rabea <aref.h.rabea@gmail.com>
2023-02-27 16:01:43 +0300

    complete Account Task and begin in css

--- controllers/account.js
+++ controllers/account.js
@@ -20,14 +20,30 @@ exports.account_create_get = (req, res) => {
 exports.create_account_post = (req, res) => {
     console.log(req.session.passport.user);
     let account = new Account(req.body);
+    var newCurr = req.body.currency;
+
     account.user = req.session.passport.user
 
-    account.save()
-        .then(() => {
-            console.log(account.user);
-            res.redirect('/account/index')
-        }
-        )
+    var account1
+    Account.find({user: req.session.passport.user})
+        .then((acc) => {
+            console.log(acc);
+            acc.forEach(function(acc1) {
+                if(acc1.currency == newCurr) {
+                    res.send('You already have an account with this currency')
+            }});
+//             account1 = acc
+//             for(let i = 0; i< account1.length; i++){
+//                 if(account1.currency == account1.currency){
+//                 res.send('You already have an account with this currency')
+})
+
+// }})
+
+//             console.log(account.user);
+//             res.redirect('/account/index')
+        
+        
         .catch((err) => {
             console.log(err);
             res.send('Something went wrong in creating the account API post');
@@ -39,16 +55,50 @@ exports.create_account_post = (req, res) => {
 
 // account index
 exports.account_index_get = (req, res) => {
-    Account.find()
-   .then(accounts => {
-        res.render('account/index', {accounts})
-    })
-   .catch(err => {
-        console.log(err)
-        res.send('Something went wrong in creating the account API get');
-    })
-    
-} 
+    console.log(req.session.passport.user);
+    Account
+        .find({ user: req.session.passport.user })
+        .then((account) => {
+            console.log(account[0].accountNumber);
+            res.render('account/index', { account })
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send('احسنت')
+        })
+}
+
+// account detail
+exports.accountdetail_show_get = (req,res) => {
+    Account.findById(req.query.id)  
+  .then(account => {
+    res.render("account/detail",{account : account})
+
+  })
+  .catch(err => {
+    console.log(err)
+})
+}
+
+
+// Delete account 
+exports.accountdetail_delete_get = (req,res) => {
+    var account
+    Account.findById(req.query.id)  
+ .then((acc) => {
+    account = acc
+    if(account.balance <= 0){
+        account.deleteOne()
+    res.redirect("/account/index")
+    }
+    else{
+        res.send(' cant delete account with balance')
+    }
+  })
+ .catch(err => {
+    console.log(err)
+})
+}
 
 
 
--- routes/accounts.js
+++ routes/accounts.js
@@ -10,8 +10,19 @@ router.use(express.urlencoded({extended:true}))
 
 
 // create account
-router.get('/account/add', isLoggedIn, accountCntrol.account_create_get)
+router.get('/account/add',isLoggedIn ,accountCntrol.account_create_get)
 router.post('/account/add', accountCntrol.create_account_post)
 
+router.get('/account/index', accountCntrol.account_index_get)
+
+router.get('/account/detail' , accountCntrol.accountdetail_show_get)
+
+router.get('/account/delete', accountCntrol.accountdetail_delete_get)
+
+
+
 module.exports = router
+
+
+// 
 
