9876a6363dbed6ebb585e4a72d42cddf72eae8f4
Adulla Qassim <aboodyaq7@gmail.com>
2023-02-27 15:44:08 +0300

    finished forget password method with recovery key

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,21 @@
-const express = require('express')
-const router = express.Router()
-// const isLoggedIn = require('../lib/isLoggedIn')
+const express = require("express");
+const router = express.Router();
+const isLoggedIn = require("../lib/isLoggedIn");
 
+const authCntrl = require("../controllers/auth");
+router.use(express.urlencoded({ extended: true }));
 
-const authCntrl = require('../controllers/auth')
-router.use(express.urlencoded({ extended: true }))
+router.get("/auth/signup", authCntrl.auth_signup_get);
+router.post("/auth/signup", authCntrl.auth_signup_post);
 
-router.get('/auth/signup', authCntrl.auth_signup_get)
-router.post('/auth/signup', authCntrl.auth_signup_post)
+router.get("/auth/signin", authCntrl.auth_signin_get);
+router.post("/auth/signin", authCntrl.auth_signin_post);
 
+// router.get("/auth/signout", authCntrl.auth_signout_get);
 
+router.get("/auth/forget", authCntrl.auth_forget_get);
+router.post("/auth/forget", authCntrl.auth_forget_post);
 
-router.get('/auth/signin', authCntrl.auth_signin_get)
-router.post('/auth/signin', authCntrl.auth_signin_post)
+router.get("/auth/updatepassword", authCntrl.auth_update_get);
 
-
-router.get('/auth/signout', authCntrl.auth_signout_get)
-
-
-router.get('/auth/forget', authCntrl.auth_forget_get)
-router.post('/auth/forget', authCntrl.auth_forget_post)
-
-router.get('/auth/updatepassword', authCntrl.auth_update_get)
-
-
-module.exports = router
+module.exports = router;
