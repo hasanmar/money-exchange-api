44bef72c9718cf78f20e1f68f1f7e2b41032dffc
Aref Rabea <aref.h.rabea@gmail.com>
2023-02-28 11:34:02 +0300

    fix Account controlers

--- controllers/account.js
+++ controllers/account.js
@@ -1,104 +1,91 @@
-const Account = require('../models/Account');
-
+const Account = require("../models/Account");
 
 // create account
 // {user: {"_id": req.user.id}}
 exports.account_create_get = (req, res) => {
-    console.log(req.session.passport.user);
-
-    Account.find()
-        .then(accounts => {
-            res.render('account/add', { accounts })
-        })
-        .catch(err => {
-            console.log(err)
-            res.send('Something went wrong in creating the account API get');
-        })
-
-}
+  Account.find()
+    .then((accounts) => {
+      res.render("account/add", { accounts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("Something went wrong in creating the account API get");
+    });
+};
 
 exports.create_account_post = (req, res) => {
-    console.log(req.session.passport.user);
-    let account = new Account(req.body);
-    var newCurr = req.body.currency;
-
-    account.user = req.session.passport.user
-
-    var account1
-    Account.find({user: req.session.passport.user})
-        .then((acc) => {
-            console.log(acc);
-            acc.forEach(function(acc1) {
-                if(acc1.currency == newCurr) {
-                    res.send('You already have an account with this currency')
-            }});
-//             account1 = acc
-//             for(let i = 0; i< account1.length; i++){
-//                 if(account1.currency == account1.currency){
-//                 res.send('You already have an account with this currency')
-})
-
-// }})
-
-//             console.log(account.user);
-//             res.redirect('/account/index')
-        
-        
-        .catch((err) => {
+  let account = new Account(req.body);
+  var newCurr = req.body.currency;
+  var found = false;
+  account.user = req.session.passport.user;
+  console.log(account);
+  Account.find({ user: account.user })
+    .then((acc) => {
+      console.log(acc);
+      acc.forEach(function (acc1) {
+        if (acc1.currency == newCurr) {
+          console.log(found);
+          console.log(acc1.currency);
+          found = true;
+        }
+      });
+      if (found) {
+        console.log("fund" + found);
+        res.send("nope");
+      } else {
+        account
+          .save()
+          .then(res.redirect("/account/index"))
+          .catch((err) => {
             console.log(err);
-            res.send('Something went wrong in creating the account API post');
-        });
-}
-
-
-
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("Something went wrong in creating the account API post");
+    });
+};
 
 // account index
 exports.account_index_get = (req, res) => {
-    console.log(req.session.passport.user);
-    Account
-        .find({ user: req.session.passport.user })
-        .then((account) => {
-            console.log(account[0].accountNumber);
-            res.render('account/index', { account })
-        })
-        .catch((err) => {
-            console.log(err);
-            res.send('احسنت')
-        })
-}
+  console.log(req.session.passport.user);
+  Account.find({ user: req.session.passport.user })
+    .then((account) => {
+      console.log(account[0].accountNumber);
+      res.render("account/index", { account });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("احسنت");
+    });
+};
 
 // account detail
-exports.accountdetail_show_get = (req,res) => {
-    Account.findById(req.query.id)  
-  .then(account => {
-    res.render("account/detail",{account : account})
+exports.accountdetail_show_get = (req, res) => {
+  Account.findById(req.query.id)
+    .then((account) => {
+      res.render("account/detail", { account: account });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-  })
-  .catch(err => {
-    console.log(err)
-})
-}
-
-
-// Delete account 
-exports.accountdetail_delete_get = (req,res) => {
-    var account
-    Account.findById(req.query.id)  
- .then((acc) => {
-    account = acc
-    if(account.balance <= 0){
-        account.deleteOne()
-    res.redirect("/account/index")
-    }
-    else{
-        res.send(' cant delete account with balance')
-    }
-  })
- .catch(err => {
-    console.log(err)
-})
-}
-
-
-
+// Delete account
+exports.accountdetail_delete_get = (req, res) => {
+  var account;
+  Account.findById(req.query.id)
+    .then((acc) => {
+      account = acc;
+      if (account.balance <= 0) {
+        account.deleteOne();
+        res.redirect("/account/index");
+      } else {
+        res.send(" cant delete account with balance");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
